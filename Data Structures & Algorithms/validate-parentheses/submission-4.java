6c366894861a22adf020f0899e47d7971aa4dff4
Ahab Masud <ahabmasudsiddiqui@gmail.com>
2026-08-12 11:09:34 -0700

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque <Character> stack = new ArrayDeque<>();
+
+        for(int i =0 ; i<s.length() ;i++){
+            if ( s.charAt(i) == '('|| s.charAt(i) == '[' || s.charAt(i) == '{')
+                {
+            
+                    if (s.charAt(i)== '('){
+                        stack.push(')');
+                    }
+                    else if(s.charAt(i)== '['){
+                        stack.push(']');
+                    }
+                    else if(s.charAt(i)== '{'){
+                        stack.push('}');
+                    }
+                }
+            else{
+               if (stack.isEmpty() || stack.pop() != s.charAt(i) ){
+                    return false;
+               }
+
+            }
+              
+        }
+       return stack.isEmpty();
+
+
+    }
+ }
+
