17289afe1700e6476475f08e885269e0bcf5ddc4
Ahab Masud <ahabmasudsiddiqui@gmail.com>
2026-08-18 16:57:34 -0700

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+    String lower= s.toLowerCase().replaceAll(
+        "[^a-zA-Z0-9]", ""
+    );
+    System.out.println(lower);
+    int left = 0;
+    int right = lower.length()-1;
+
+
+    while( left < right ){
+    char first= lower.charAt(left); //w
+    char last = lower.charAt(right); //w
+
+        if( first!= last){
+            return false;
+        }
+        else{
+        left ++;
+        right --;
+        }
+    }
+
+    return true;
+    }
+  
+}
